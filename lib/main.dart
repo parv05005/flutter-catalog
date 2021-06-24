287735e62e6d455f8806b779f5bc1d5ffe02e628
parv81548 <45035480+parv81548@users.noreply.github.com>
2021-06-24 12:30:29 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_proj/pages/Home_Page.dart';
 import 'package:first_proj/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var materialApp = MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
